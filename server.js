a9a1d5af8dae3c674392da4d4f7dff1d3bc359d7
wooyeongkam <wooyeong.kam@avikus.ai>
2023-07-18 20:58:47 +0900

    capture

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 Stream = require("node-rtsp-stream");
 stream = new Stream({
   name: "name",
-  streamUrl: "rtsp://localhost:8554/mystream",
+  streamUrl: "http://localhost:8889/mystream",
   wsPort: 9999,
   ffmpegOptions: {
     "-stats": "",
     "-r": 30,
   },
 });
+
+// docker run --rm -it \
+// -e MTX_PROTOCOLS=tcp \
+// -p 8554:8554 \
+// -p 1935:1935 \
+// -p 8888:8888 \
+// -p 8889:8889 \
+// --network=host
+// bluenviron/mediamtx
